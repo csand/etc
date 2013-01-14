0f5f5c435a928724b80c0c56dffa52d16d02a952
Sam Anderson <self@coarsesand.net>
2013-01-14 02:36:06 -0500

    vim: Added section for vundle to setup.zsh

--- setup.zsh
+++ setup.zsh
@@ -12,3 +12,9 @@ done
 scripts=($PWD/scripts/*)
 mkdir -p "$BINDIR"
 "$ln_cmd $scripts $BINDIR"
+
+if [[ ! -d $DOTDIR/.vim/bundle/vundle ]]; then
+  mkdir -p ""$DOTDIR/.vim/bundle
+  git clone git://github.com/gmarik/vundle.git "$DOTDIR/.vim/bundle/vundle"
+  vim +BundleInstall +qall
+fi
