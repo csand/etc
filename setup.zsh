c0218d933ea68e5b5d1af5879e853b982d5d3fd0
Sam Anderson <self@coarsesand.net>
2013-01-15 14:29:00 -0500

    Forgot to update the setup script extension

--- setup.zsh
+++ setup.zsh
@@ -5,8 +5,8 @@ DOTDIR="${DOTDIR:-$HOME}"
 BINDIR="${BINDIR:-$HOME/bin}"
 ln_cmd='ln -sf'
 
-for dotfile in $PWD/^(README.md|setup.sh|scripts); do
-  "$ln_cmd $dotfile $DOTDIR/.${dotfile:t}"
+for dotfile in $PWD/^(README.md|setup.zsh|scripts); do
+  $ln_cmd $dotfile $DOTDIR/.${dotfile:t}
 done
 
 scripts=($PWD/scripts/*)
@@ -14,7 +14,7 @@ mkdir -p "$BINDIR"
 "$ln_cmd $scripts $BINDIR"
 
 if [[ ! -d $DOTDIR/.vim/bundle/vundle ]]; then
-  mkdir -p ""$DOTDIR/.vim/bundle
-  git clone git://github.com/gmarik/vundle.git "$DOTDIR/.vim/bundle/vundle"
+  mkdir -p $DOTDIR/.vim/bundle
+  git clone git://github.com/gmarik/vundle.git $DOTDIR/.vim/bundle/vundle
   vim +BundleInstall +qall
 fi
