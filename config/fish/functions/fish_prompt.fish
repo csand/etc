007b38f69a29237820ecd6a51b5ba6bfd7aeebb2
csand <sam@csand.net>
2014-04-21 16:56:54 -0400

    More prompt work

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,50 +1,50 @@
-function with_range
-    for i in $argv
-        echo $i
-    end
-end
-
 function fish_prompt --description "Prompt, based on jekor's"
-
-    set -g ___fish_git_prompt_color_dirtystate_done ' '
-    set -g ___fish_git_prompt_char_dirtystate  'dirty'
-    set -g ___fish_git_prompt_char_stagedstate 'staged'
+    set last_status $status
 
     set color_good (set_color green)
     set color_bad  (set_color red)
     set color_user (set_color green)
     set color_cwd  (set_color blue)
     set color_venv (set_color magenta)
-    set color_char (set_color yellow --bold)
+    set color_char (set_color red --bold)
     set color_git_branch (set_color cyan)
     set color_git_dirty  (set_color red)
     set color_git_staged (set_color yellow)
 
     # status
-    set last_status $status
-    if test $last_status -ne 0
-        set prompt_status "$color_bad"✗
+    if test $last_status -eq 0
+        set prompt_status "$color_good"✓" "
     else
-        set prompt_status "$color_good"✓
+        set prompt_status "$color_bad"✗" $status "
     end
-    set prompt_status $prompt_status' '
 
     # user
     set prompt_user $color_user(whoami)' '
 
     # git
-    set -gx __fish_git_prompt_showdirtystate 1
-    set _git_prompt (with_range (__fish_git_prompt '%s'))
-    # echo $_git_prompt[1]
-    set prompt_branch $_git_prompt[1]
-    switch _git_prompt[2]
-        case 'dirty'
-            set prompt_branch $color_git_dirty$prompt_branch
-        case 'staged'
-            set prompt_branch $color_git_staged$prompt_branch
-        case '*'
-            set prompt_branch $color_git_branch$prompt_branch
-    end
+    # force the fish git prompt to give useful output
+    # set -g ___fish_git_prompt_color_dirtystate_done ' '
+    # set -g ___fish_git_prompt_char_dirtystate  'dirty'
+    # set -g ___fish_git_prompt_char_stagedstate 'staged'
+    # set -gx __fish_git_prompt_showdirtystate 1
+
+    # set prompt_git ''
+    # set _git_prompt (splitline (__fish_git_prompt '%s'))
+    # if test -n "$_git_prompt"
+    #     set branch $_git_prompt[1]
+    #     set _git_status $_git_prompt[2]
+    #     if test -n $_git_status
+    #         switch $_git_status
+    #             case dirty
+    #                 set prompt_git (echo -sn (set_color red) $branch)
+    #             case staged
+    #                 set prompt_git (echo -sn (set_color yellow) $branch)
+    #         end
+    #     else
+    #         set prompt_git $branch
+    #     end
+    #     set prompt_git (printf '%s[%s%s] ' $color_git_branch $prompt_git $color_git_branch)
+    # end
 
     # virtualenv
     set prompt_venv ''
@@ -55,6 +55,6 @@ function fish_prompt --description "Prompt, based on jekor's"
     set prompt_char "$color_char"λ' '
 
     echo -n -s $prompt_status $prompt_user \
-        $color_cwd(pwd_trunc.py) ' ' (__fish_git_prompt '[%s] ') \
+        $color_cwd(pwd_trunc.py) ' ' $prompt_git \
         $prompt_venv $prompt_char (set_color normal)
 end
