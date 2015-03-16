8e0737df61594499f0457ebbae26dd1e6dd1c57a
csand <sam@csand.net>
2015-03-16 11:51:33 -0400

    scripts: make pwd_func.py work on python 3

--- scripts/pwd_trunc.py
+++ scripts/pwd_trunc.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
-
+from __future__ import print_function
 import os
-
 
 def truncate_cwd(begin, end):
     home_dir = os.environ['HOME']
@@ -10,6 +9,5 @@ def truncate_cwd(begin, end):
                     if (begin + end + 1) < len(comps)
                     else comps)
 
-
 if __name__ == '__main__':
-    print truncate_cwd(1, 2)
+    print(truncate_cwd(1, 2))
