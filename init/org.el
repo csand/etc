23f1095ca1e4db1f103e0ba7576d7bbad0715839
Sam Anderson <sam@csand.net>
2017-10-19 17:45:24 -0400

    Add evil-org

--- init/org.el
+++ init/org.el
@@ -7,6 +7,14 @@
   :config
   (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
 
+(use-package evil-org
+  :after org
+  :pin melpa
+  :config
+  ;; (setq evil-org-special-o/O t)
+  (add-hook 'org-mode-hook 'evil-org-mode)
+  (add-hook 'evil-org-mode-hook (lambda () (evil-org-set-key-theme))))
+
 (setq org-directory "~/Dropbox/Org"
       org-default-notes-file (concat org-directory "/inbox.org")
       org-agenda-files (concat org-directory "/agenda.org")
