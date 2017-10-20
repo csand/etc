52601f85c974ae8240d0c33828224882a1469d05
Sam Anderson <sam@csand.net>
2017-10-19 22:59:24 -0400

    Add telephone-line

--- init/packages.el
+++ init/packages.el
@@ -143,3 +143,24 @@
   :if (eq system-type 'darwin)
   :config
   (exec-path-from-shell-initialize))
+
+(use-package telephone-line
+  :pin melpa
+  :config
+  (setq telephone-line-height (floor (* my-font-height 1.4) 10)
+        telephone-line-primary-left-separator 'telephone-line-cubed-right
+        telephone-line-secondary-left-separator 'telephone-line-cubed-hollow-right
+        telephone-line-secondary-right-separator 'telephone-line-cubed-hollow-left
+        telephone-line-primary-right-separator 'telephone-line-cubed-left
+        telephone-line-lhs
+        '((evil   . (telephone-line-evil-tag-segment))
+          (accent . (telephone-line-vc-segment
+                     telephone-line-erc-modified-channels-segment
+                     telephone-line-process-segment))
+          (nil    . (telephone-line-buffer-segment)))
+        telephone-line-rhs
+        '((nil    . (telephone-line-misc-info-segment
+                     telephone-line-airline-position-segment
+                     telephone-line-simple-minor-mode-segment))
+          (accent . (telephone-line-major-mode-segment))))
+  (telephone-line-mode 1))
