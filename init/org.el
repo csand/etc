3d74c3d946287ac5670ab9d187c1b05064a4fbfb
Sam Anderson <sam@csand.net>
2017-11-01 17:50:16 -0400

    Configure org capture and agenda files

--- init/org.el
+++ init/org.el
@@ -1,32 +1,36 @@
 ;;; org.el --- Org-mode config and enhancements
 
+(defvar org-directory "~/Dropbox/Org")
+
+(defun org-file (filename)
+  (expand-file-name filename org-directory))
+
+(defvar org-agenda-file (org-file "agenda.org"))
+(defvar org-inbox-file  (org-file "inbox.org"))
+
 (use-package org
   :mode ("\\.org\\'" . org-mode)
   :init
-  (defun setup-org-fill ()
-    (set-fill-column 80)
-    (auto-fill-mode 1))
+  (progn
+    (setq org-agenda-files `(,org-agenda-file ,org-inbox-file)
+          org-export-coding-system 'utf-8
+          org-refile-targets '((nil :maxlevel . 9)
+                               (org-inbox-file :level . 1))
+          org-src-tab-acts-natively t
+          org-startup-folded nil
+          org-tags-column 80)
+    (setq org-capture-templates
+          '(("t" "Todo" entry (file+headline org-inbox-file "Tasks")
+             "* TODO %i%?")))
+    (add-hook 'org-mode-hook 'auto-fill-mode))
   :config
-  (setq
-   org-directory "~/Dropbox/Org"
-   org-agenda-files (concat org-directory "/agenda.org")
-   org-default-notes-file (concat org-directory "/inbox.org"))
-  (setq
-   org-export-coding-system 'utf-8
-   org-src-tab-acts-natively t
-   org-tags-column 80
-   org-startup-folded nil)
-  (add-to-list 'org-src-lang-modes '("javascript" . js2))
-  (add-hook 'org-mode-hook #'setup-org-fill))
-
-(use-package org-bullets
-  :config
-  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
+  (add-to-list 'org-src-lang-modes '("javascript" . js2)))
 
 (use-package evil-org
   :after org
-  :pin melpa
   :diminish evil-org-mode
-  :config
-  (add-hook 'org-mode-hook 'evil-org-mode)
-  (add-hook 'evil-org-mode-hook (lambda () (evil-org-set-key-theme))))
+  :pin melpa
+  :init
+  (progn
+    (add-hook 'org-mode-hook 'evil-org-mode)
+    (add-hook 'evil-org-mode-hook 'evil-org-set-key-theme)))
