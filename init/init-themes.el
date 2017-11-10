cad391f35e5012d4ffe138af18f7913ccce3adeb
Sam Anderson <sam@csand.net>
2017-11-09 21:40:52 -0500

    More themes, why not?

--- init/init-themes.el
+++ init/init-themes.el
@@ -1,7 +1,27 @@
 ;;; init-themes.el --- Whatever themes I feel like installing
 
-(use-package dracula-theme :defer t)
-(use-package gruvbox-theme :defer t)
-(use-package leuven-theme :defer t)
+(use-package darktooth-theme
+  :ensure t
+  :defer t)
+
+(use-package dracula-theme
+  :ensure t
+  :defer t)
+
+(use-package gruvbox-theme
+  :ensure t
+  :defer t)
+
+(use-package leuven-theme
+  :ensure t
+  :defer t)
+
+(use-package moe-theme
+  :ensure t
+  :defer t)
+
+(use-package solarized-theme
+  :ensure t
+  :defer t)
 
 (provide 'init-themes)
