7439835339764bb577161a48d783666365dde814
Sam Anderson <sam@csand.net>
2017-11-14 17:31:57 -0500

    Add more ivy bindings

--- init/init-ivy.el
+++ init/init-ivy.el
@@ -3,26 +3,36 @@
 (use-package ivy
   :ensure t
   :diminish ivy-mode
+  :init
+  (ivy-mode 1)
+  :bind
+  (:map ivy-mode-map
+        ("C-'" . ivy-avy))
   :config
   (progn
-    (setq ivy-height 15
-          ivy-initial-inputs-alist nil)
-    (ivy-mode 1)))
+    (setq ivy-count-format "(%d/%d) "
+          ivy-height 18
+          ivy-initial-inputs-alist nil
+          ivy-use-virtual-buffers t)))
 
 (use-package counsel
   :ensure t
   :after ivy
+  :bind*
+  (("M-x"     . counsel-M-x)
+   ("C-x C-f" . counsel-find-file)
+   ("C-x C-r" . counsel-recentf)
+   ("C-h f"   . counsel-describe-function)
+   ("C-h v"   . counsel-describe-variable))
   :config
-  (when (eq system-type 'windows-nt)
-    (setq counsel-ag-base-command "pt /e /nocolor /nogroup"))
-  :bind
-  ("M-x" . counsel-M-x)
-  ("C-h f" . counsel-describe-function)
-  ("C-h v" . counsel-describe-variable))
+  (when is-windows
+    (setq counsel-ag-base-command "pt /e /nocolor /nogroup")))
 
 (use-package swiper
   :ensure t
-  :after ivy)
+  :after ivy
+  :bind*
+  (("C-s" . swiper)))
 
 (use-package ivy-hydra
   :ensure t
