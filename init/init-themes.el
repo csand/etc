dc2fc98503baab0a453d485e282b40be087ebbf2
Sam Anderson <sam@csand.net>
2018-09-27 22:48:18 -0400

    Many, many themes

--- init/init-themes.el
+++ init/init-themes.el
@@ -1,15 +1,5 @@
 ;;; init-themes.el --- Whatever themes I feel like installing
 
-(use-package darktooth-theme :defer t)
-
-(use-package dracula-theme :defer t)
-
-(use-package gruvbox-theme :defer t)
-
-(use-package leuven-theme :defer t)
-
-(use-package moe-theme :defer t)
-
 (use-package solarized-theme
   :defer t
   :custom
@@ -18,39 +8,40 @@
   (solarized-use-less-bold t)
   (solarized-use-more-italic nil))
 
-(use-package flatui-theme :defer t)
-
-(use-package tao-theme :defer t)
-
-(use-package material-theme :defer t)
-
-(use-package color-theme-sanityinc-tomorrow :defer t)
-
-(use-package afternoon-theme :defer t)
-
-(use-package atom-one-dark-theme :defer t)
-
 (use-package nord-theme
   :defer t
   :init
   (setq nord-comment-brightness 20))
 
-(use-package sublime-themes :defer t)
-
-(use-package zerodark-theme :defer t)
-
-(use-package challenger-deep-theme :defer t)
-
-(use-package majapahit-theme :defer t)
-
+(use-package afternoon-theme :defer t)
+(use-package ample-theme :defer t)
 (use-package arjen-grey-theme :defer t)
-
+(use-package atom-one-dark-theme :defer t)
+(use-package badwolf-theme :defer t)
+(use-package challenger-deep-theme :defer t)
+(use-package color-theme-sanityinc-tomorrow :defer t)
+(use-package darktooth-theme :defer t)
+(use-package dracula-theme :defer t)
+(use-package eink-theme :defer t)
+(use-package eziam-theme :defer t)
+(use-package flatui-theme :defer t)
 (use-package goose-theme :defer t)
-
-(use-package oldlace-theme :defer t)
-
+(use-package grayscale-theme :defer t)
 (use-package greymatters-theme :defer t)
-
+(use-package gruvbox-theme :defer t)
+(use-package leuven-theme :defer t)
+(use-package majapahit-theme :defer t)
+(use-package material-theme :defer t)
+(use-package moe-theme :defer t)
+(use-package monotropic-theme :defer t)
+(use-package nordless-theme :defer t)
+(use-package oldlace-theme :defer t)
+(use-package plain-theme :defer t)
 (use-package poet-theme :defer t)
+(use-package punpun-theme :defer t)
+(use-package sublime-themes :defer t)
+(use-package tao-theme :defer t)
+(use-package white-sand-theme :defer t)
+(use-package zerodark-theme :defer t)
 
 (provide 'init-themes)
