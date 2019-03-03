33b44722725b932eb831bad367844dbefcdc4f9e
Sam Anderson <sam@csand.net>
2019-03-03 16:35:17 -0500

    Configure org mode heading font families

--- init/init-org.el
+++ init/init-org.el
@@ -23,44 +23,65 @@
   :ensure org-plus-contrib
   :pin org
   :mode ("\\.org\\'" . org-mode)
+  :custom-face
+  (org-document-title ((t (:family "Triplicate C4"))))
+  (org-level-1 ((t (:family "Triplicate C4"))))
+  (org-level-2 ((t (:family "Triplicate C4"))))
+  (org-level-3 ((t (:family "Triplicate C4"))))
+  (org-level-4 ((t (:family "Triplicate C4"))))
+  (org-level-5 ((t (:family "Triplicate C4"))))
+  (org-level-6 ((t (:family "Triplicate C4"))))
+  (org-level-7 ((t (:family "Triplicate C4"))))
+  (org-level-8 ((t (:family "Triplicate C4"))))
   :init
-  (progn
-    (setq org-agenda-files `(,org-inbox-file
-                             ,org-projects-file
-                             ,org-work-file)
-          org-export-coding-system 'utf-8
-          org-refile-targets '((nil :maxlevel . 9)
-                               (org-inbox-file :level . 1)
-                               (org-projects-file :level . 1))
-          org-src-tab-acts-natively t
-          org-startup-folded nil
-          org-tags-column -80)
-    (setq org-capture-templates `(,org-todo-template
-                                  ,org-work-item-template))
-    (setq org-todo-keywords '((sequence "TODO(t)" "WAITING(w!)" "|" "DONE(d)" "SOMEDAY(s)" "WONTDO(n!)")))
-    (add-hook 'org-mode-hook #'visual-line-mode))
+  (setq org-agenda-files `(,org-inbox-file
+                           ,org-projects-file
+                           ,org-work-file)
+        org-export-coding-system 'utf-8
+        org-refile-targets '((nil :maxlevel . 9)
+                             (org-inbox-file :level . 1)
+                             (org-projects-file :level . 1))
+        org-src-tab-acts-natively t
+        org-startup-folded nil
+        org-tags-column -80
+        org-hide-emphasis-markers t)
+  (setq org-capture-templates `(,org-todo-template
+                                ,org-work-item-template))
+  (setq org-todo-keywords '((sequence
+                             "TODO(t)"
+                             "WAITING(w!)"
+                             "|"
+                             "DONE(d)"
+                             "SOMEDAY(s)"
+                             "WONTDO(n!)")))
   :config
-  (progn
-    (define-follower-key "c" 'org-capture)
-    (define-major-mode-follower-key
-      :keymaps 'org-mode-map
-      "," 'org-ctrl-c-ctrl-c
-      ":" 'org-set-tags
-      "'" 'org-edit-special
-      "r" 'org-refile
-      "t" 'org-todo)
-    (general-define-key
-     :keymaps 'org-mode-map
-     :states 'normal
-     "t" 'org-todo)
-    (define-major-mode-follower-key
-      :keymaps 'org-capture-mode-map
-      "," 'org-capture-finalize
-      "a" 'org-capture-kill
-      "c" 'org-capture-finalize
-      "k" 'org-capture-kill
-      "r" 'org-capture-refile
-      "w" 'org-capture-refile)
-    (add-to-list 'org-src-lang-modes '("javascript" . js2))))
+  (define-follower-key "c" 'org-capture)
+  (define-major-mode-follower-key
+    :keymaps 'org-mode-map
+    "," 'org-ctrl-c-ctrl-c
+    ":" 'org-set-tags
+    "'" 'org-edit-special
+    "r" 'org-refile
+    "t" 'org-todo)
+  (general-define-key
+   :keymaps 'org-mode-map
+   :states 'normal
+   "t" 'org-todo)
+  (define-major-mode-follower-key
+    :keymaps 'org-capture-mode-map
+    "," 'org-capture-finalize
+    "a" 'org-capture-kill
+    "c" 'org-capture-finalize
+    "k" 'org-capture-kill
+    "r" 'org-capture-refile
+    "w" 'org-capture-refile)
+  (add-to-list 'org-src-lang-modes '("javascript" . js2))
+  (org-babel-do-load-languages 'org-babel-load-languages
+                               '((awk . t)))
+  (set-face-attribute 'org-code nil
+                      :family "Triplicate T4")
+  (require 'org-tempo)
+  (add-to-list 'org-structure-template-alist
+               '("js" . "src javascript")))
 
 (provide 'init-org)
