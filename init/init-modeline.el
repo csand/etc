32da9ba71c52ee921a0624ef6a1d0f0c180b228f
Sam Anderson <sam@csand.net>
2019-03-03 16:47:29 -0500

    Kill old modeline and just use sml+minions

--- init/init-modeline.el
+++ init/init-modeline.el
@@ -2,57 +2,69 @@
 
 (require 'init-extras)
 
-(defconst mode-line-window-number
-  '(:eval (concat "W" (winum-get-number-string))))
+(setq ns-use-srgb-colorspace nil)
 
-(defconst mode-line-buffer-info
-  '(:eval (concat
-           (if (buffer-modified-p) "*" "-")
-           " "
-           (propertize (buffer-name) 'face '(:weight bold))
-           )))
+(use-package smart-mode-line
+  :custom
+  (sml/theme 'respectful)
+  :config
+  (sml/setup))
 
-(defconst mode-line-point-position
-  '(:eval (propertize (format-mode-line "%lL,%cC") 'face '(:weight light))))
 
-(defconst mode-line-evil-tag
-  '(:eval (cond
-           ((not (evil-visual-state-p)) (upcase (symbol-name evil-state)))
-           ((eq evil-visual-selection 'block) "V-BLOCK")
-           ((eq evil-visual-selection 'line) "V-LINE")
-           (t "VISUAL")
-           )))
+(use-package minions
+  :config (minions-mode 1))
 
-(defconst mode-line-mode-list
-  '(:eval (concat
-           "("
-           (propertize mode-name 'face '(:weight bold))
-           (format-mode-line minor-mode-alist)
-           ")"
-           )))
+;; (defconst mode-line-window-number
+;;   '(:eval (concat "W" (winum-get-number-string))))
 
-(defconst mode-line-vc-info
-  '(:eval (concat
-           ""
-           vc-mode
-           " ("
-           (symbol-name (vc-state (buffer-file-name)))
-           ")"
-           )))
+;; (defconst mode-line-buffer-info
+;;   '(:eval (concat
+;;            (if (buffer-modified-p) "*" "-")
+;;            " "
+;;            (propertize (buffer-name) 'face '(:weight bold))
+;;            )))
 
-(setq-default mode-line-format
-              '(:eval (concat
-                       (format-mode-line mode-line-front-space)
-                       (format-mode-line mode-line-window-number)
-                       " "
-                       (format-mode-line mode-line-buffer-info)
-                       (if evil-mode evil-mode-line-tag)
-                       (format-mode-line mode-line-misc-info)
-                       (format-mode-line mode-line-mode-list)
-                       " "
-                       (format-mode-line mode-line-point-position)
-                       (format-mode-line mode-line-end-spaces)
-                       )))
+;; (defconst mode-line-point-position
+;;   '(:eval (propertize (format-mode-line "%lL,%cC") 'face '(:weight light))))
+
+;; (defconst mode-line-evil-tag
+;;   '(:eval (cond
+;;            ((not (evil-visual-state-p)) (upcase (symbol-name evil-state)))
+;;            ((eq evil-visual-selection 'block) "V-BLOCK")
+;;            ((eq evil-visual-selection 'line) "V-LINE")
+;;            (t "VISUAL")
+;;            )))
+
+;; (defconst mode-line-mode-list
+;;   '(:eval (concat
+;;            "("
+;;            (propertize mode-name 'face '(:weight bold))
+;;            (format-mode-line minor-mode-alist)
+;;            ")"
+;;            )))
+
+;; (defconst mode-line-vc-info
+;;   '(:eval (concat
+;;            ""
+;;            vc-mode
+;;            " ("
+;;            (symbol-name (vc-state (buffer-file-name)))
+;;            ")"
+;;            )))
+
+;; (setq-default mode-line-format
+;;               '(:eval (concat
+;;                        (format-mode-line mode-line-front-space)
+;;                        (format-mode-line mode-line-window-number)
+;;                        " "
+;;                        (format-mode-line mode-line-buffer-info)
+;;                        (if evil-mode evil-mode-line-tag)
+;;                        (format-mode-line mode-line-misc-info)
+;;                        (format-mode-line mode-line-mode-list)
+;;                        " "
+;;                        (format-mode-line mode-line-point-position)
+;;                        (format-mode-line mode-line-end-spaces)
+;;                        )))
 
 ;; Value:
 ;; ("%e"
