7a69052174b5ce054c022b565a085555caa94482
Sam Anderson <sam@csand.net>
2019-06-06 15:22:54 -0400

    Add eshell-here function

--- init/init-funcs.el
+++ init/init-funcs.el
@@ -68,6 +68,24 @@ Repeated invocations toggle between the two most recently open buffers."
   (interactive)
   (switch-to-buffer (get-buffer-create "*scratch*")))
 
+(defun eshell-here ()
+  "Opens up a new shell in the directory associated with the
+current buffer's file. The eshell is renamed to match that
+directory to make multiple eshell windows easier."
+  (interactive)
+  (let* ((parent (if (buffer-file-name)
+                     (file-name-directory (buffer-file-name))
+                   default-directory))
+         (height (/ (window-total-height) 3))
+         (name   (car (last (split-string parent "/" t)))))
+    (split-window-vertically (- height))
+    (other-window 1)
+    (eshell "new")
+    (rename-buffer (concat "*eshell: " name "*"))
+
+    (insert (concat "ls"))
+    (eshell-send-input)))
+
 (defun assq-delete-all-equal (key alist)
   "Like `assq-delete-all', but uses `equal' instead of `eq' so it can match
 string keys in the alist."
