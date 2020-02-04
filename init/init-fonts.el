acb239645f4c985d500e82e760cd2beb0fd12b74
Sam Anderson <sam@csand.net>
2020-02-03 23:54:14 -0500

    Add personal functions for changing fonts

--- init/init-fonts.el
+++ init/init-fonts.el
@@ -1,25 +1,32 @@
 ;;; init-fonts.el --- Font face configuration
 
-;; Pragmata has a very high baseline, so place the
-;; underline at the descent line instead
+;; Pragmata Pro has a very high baseline, so place
+;; the underline at the descent line instead.
 (setq x-underline-at-descent-line t)
 
-(setq sand-font-height 130)
+(setq-default line-spacing nil)
+
+(defvar sand-font-height 140)
+
 (set-face-attribute 'default nil
-                    :family "PragmataPro"
                     :height sand-font-height
                     :width 'normal
                     :weight 'normal)
 
-;; Set the fixed-pitch font. Mostly affects markdown mode source blocks
-(set-face-attribute 'fixed-pitch nil
-                    :family "PragmataPro Mono")
+(defun sand/set-font-family (family &optional face)
+  (interactive "sFont family: ")
+  (set-face-attribute (or face 'default) nil :family family))
 
-(set-face-attribute 'fixed-pitch-serif nil
-                    :family "Triplicate T4c")
+(defun sand/set-font-height (&optional height face)
+  (interactive (list (read-number (format "Font height: ") sand-font-height)))
+  (set-face-attribute (or face 'default) nil
+                      :height (or height sand-font-height)))
 
-(set-face-attribute 'variable-pitch nil
-                    :family "Triplicate T4"
-                    :height sand-font-height)
+(sand/set-font-family "PragmataPro")
+
+(sand/set-font-family "PragmataPro Mono" 'fixed-pitch) ;; Mostly affects markdown mode source blocks
+
+(sand/set-font-family "Triplicate T4c" 'fixed-pitch-serif)
+(sand/set-font-family "Triplicate T4" 'variable-pitch)
 
 (provide 'init-fonts)
