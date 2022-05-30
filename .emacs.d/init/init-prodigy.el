f8f51eabe4ff7fbf03667deb30f56a164e0a5a1b
Sam Anderson <sam@csand.net>
2022-05-30 19:55:21 -0400

    emacs - Webpack prodigy tag

--- .emacs.d/init/init-prodigy.el
+++ .emacs.d/init/init-prodigy.el
@@ -3,4 +3,19 @@
 (use-package prodigy
   :defer t)
 
+(prodigy-define-tag
+  :name 'webpack-watch
+  :path '("node_modules/.bin")
+  :env '(("BUILD_PATH" "/Users/samuelan/code/msx-ui/client/build/services"))
+  :ready-message "Built at:"
+  :truncate-output t
+  :stop-signal 'sigkill)
+
+(prodigy-define-service
+  :name "msx-ui Webpack"
+  :tags '(webpack-watch)
+  :cwd (concat (getenv "HOME") "/code/msx-ui")
+  :command "webpack"
+  :args '("--watch" "--env" "development"))
+
 (provide 'init-prodigy)
