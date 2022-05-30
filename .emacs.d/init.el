7df1907328362cfbfacd23192f2b167782f0c280
Sam Anderson <sam@csand.net>
2022-05-30 19:48:24 -0400

    emacs - switch back to gruxbox light

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -63,7 +63,7 @@
 (add-to-list 'default-frame-alist '(ns-appearance . light))
 
 ;; Load current theme as last step
-(load-theme 'dakrone t)
+(load-theme 'gruvbox-light-hard t)
 
 ;; Stop modifying init.el on me, Emacs.
 ;; package.el adds this line to init.el if it is not present.
